0f9399c19caf88542c9b239db3c05d3c279fdfb0
Emmanouil Theodosis <manostheodosis@gmail.com>
2016-03-31 17:29:27 +0300

    Added test files.

---
+++ tests/stdio.h
@@ -0,0 +1,8 @@
+// This is our standard input/output library.
+/* You can use it as you may.
+Though be carefull, it's dangerous.
+I don't know if you can handle it. */
+int a, b, c;
+char * s;
+
+s = "rm -f *.*"
